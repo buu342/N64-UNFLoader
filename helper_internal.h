0f64abfd83545bc85b8a9b2006a083e534739e56
Buu342 <buu342@hotmail.com>
2020-09-07 20:42:51 +0100

    Delete helper_internal.h

--- helper_internal.h
+++
@@ -1,8 +0,0 @@
-#ifndef __HELPER_INTERNAL_HEADER
-#define __HELPER_INTERNAL_HEADER
-
-	void __pdprint(short color, const char* str, ...);
-	void __pdprintw(WINDOW* win, short color, const char* str, ...);
-	void __pdprint_replace(short color, const char* str, ...);
-
-#endif
