7faceb88ac76586471839b5d0f328771f1a15a57
buu342 <buu342@hotmail.com>
2020-11-23 16:53:21 +0000

    Added Example 4

---
+++ Examples/4. Command Interpreter/helper.h
@@ -0,0 +1,11 @@
+#ifndef UNFLEX_HELPER_H
+#define UNFLEX_HELPER_H
+
+    /*********************************
+                Functions
+    *********************************/
+
+    void rcp_init();
+    void fb_clear(u8 r, u8 g, u8 b);
+
+#endif
