838a8294b23882e940c0eea585de9609acba1942
Robin Jones <networkfusion@users.noreply.github.com>
2020-12-04 23:17:22 +0000

    Downgrade FTDI to XP version

--- UNFLoader/Include/ftd2xx.h
+++ UNFLoader/Include/ftd2xx.h
@@ -41,10 +41,6 @@ kernel & user mode
 #ifndef FTD2XX_H
 #define FTD2XX_H
 
-#ifdef _WIN32
-// Compiling on Windows
-#include <windows.h>
-
 // The following ifdef block is the standard way of creating macros
 // which make exporting from a DLL simpler.  All files within this DLL
 // are compiled with the FTD2XX_EXPORTS symbol defined on the command line.
@@ -55,24 +51,10 @@ kernel & user mode
 
 #ifdef FTD2XX_EXPORTS
 #define FTD2XX_API __declspec(dllexport)
-#elif defined(FTD2XX_STATIC)
-// Avoid decorations when linking statically to D2XX.
-#define FTD2XX_API
-// Static D2XX depends on these Windows libs:
-#pragma comment(lib, "setupapi.lib")
-#pragma comment(lib, "advapi32.lib")
-#pragma comment(lib, "user32.lib")
 #else
 #define FTD2XX_API __declspec(dllimport)
 #endif
 
-#else // _WIN32
-// Compiling on non-Windows platform.
-#include "WinTypes.h"
-// No decorations needed.
-#define FTD2XX_API
-
-#endif // _WIN32
 
 typedef PVOID	FT_HANDLE;
 typedef ULONG	FT_STATUS;
@@ -114,10 +96,6 @@ enum {
 #define FT_OPEN_BY_SERIAL_NUMBER	1
 #define FT_OPEN_BY_DESCRIPTION		2
 #define FT_OPEN_BY_LOCATION			4
-
-#define FT_OPEN_MASK (FT_OPEN_BY_SERIAL_NUMBER | \
-                      FT_OPEN_BY_DESCRIPTION | \
-                      FT_OPEN_BY_LOCATION)
 
 //
 // FT_ListDevices Flags (used in conjunction with FT_OpenEx Flags
@@ -220,14 +198,7 @@ enum {
 	FT_DEVICE_2232H,
 	FT_DEVICE_4232H,
 	FT_DEVICE_232H,
-	FT_DEVICE_X_SERIES,
-	FT_DEVICE_4222H_0,
-	FT_DEVICE_4222H_1_2,
-	FT_DEVICE_4222H_3,
-    FT_DEVICE_4222_PROG,
-    FT_DEVICE_900,
-    FT_DEVICE_930,
-    FT_DEVICE_UMFTPD3A,
+	FT_DEVICE_X_SERIES
 };
 
 //
@@ -284,8 +255,8 @@ enum {
 //
 
 #define FT_X_SERIES_CBUS_TRISTATE			0x00	//	Tristate
-#define FT_X_SERIES_CBUS_TXLED				0x01	//	Tx LED
-#define FT_X_SERIES_CBUS_RXLED				0x02	//	Rx LED
+#define FT_X_SERIES_CBUS_RXLED				0x01	//	Tx LED
+#define FT_X_SERIES_CBUS_TXLED				0x02	//	Rx LED
 #define FT_X_SERIES_CBUS_TXRXLED			0x03	//	Tx and Rx LED
 #define FT_X_SERIES_CBUS_PWREN				0x04	//	Power Enable
 #define FT_X_SERIES_CBUS_SLEEP				0x05	//	Sleep
@@ -317,18 +288,6 @@ enum {
 extern "C" {
 #endif
 
-
-#ifdef FTD2XX_STATIC
-    FTD2XX_API
-        FT_STATUS WINAPI FT_Initialise(
-        void
-        );
-
-    FTD2XX_API
-        void WINAPI FT_Finalise(
-        void
-        );
-#endif // FTD2XX_STATIC
 
 	FTD2XX_API
 		FT_STATUS WINAPI FT_Open(
@@ -1026,29 +985,6 @@ extern "C" {
 		ULONG ulDeadmanTimeout
 		);
 
-#ifndef _WIN32
-	// Extra functions for non-Windows platforms to compensate
-	// for lack of .INF file to specify Vendor and Product IDs.
-
-	FTD2XX_API
-		FT_STATUS FT_SetVIDPID(
-		DWORD dwVID, 
-		DWORD dwPID
-		);
-			
-	FTD2XX_API
-		FT_STATUS FT_GetVIDPID(
-		DWORD * pdwVID, 
-		DWORD * pdwPID
-		);
-
-	FTD2XX_API
-		FT_STATUS WINAPI FT_GetDeviceLocId(
-		FT_HANDLE ftHandle,
-		LPDWORD lpdwLocId
-		);
-#endif // _WIN32        
-
 	FTD2XX_API
 		FT_STATUS WINAPI FT_GetDeviceInfo(
 		FT_HANDLE ftHandle,
@@ -1181,7 +1117,7 @@ extern "C" {
 		WORD XoffLim;				/* Transmit X-OFF threshold				*/
 		BYTE ByteSize;				/* Number of bits/byte, 4-8				*/
 		BYTE Parity;				/* 0-4=None,Odd,Even,Mark,Space			*/
-		BYTE StopBits;				/* FT_STOP_BITS_1 or FT_STOP_BITS_2		*/
+		BYTE StopBits;				/* 0,1,2 = 1, 1.5, 2					*/
 		char XonChar;				/* Tx and Rx X-ON character				*/
 		char XoffChar;				/* Tx and Rx X-OFF character			*/
 		char ErrorChar;				/* Error replacement char				*/
@@ -1395,47 +1331,6 @@ extern "C" {
 		DWORD *dwRxBytes
 		);
 
-	FTD2XX_API
-		FT_STATUS WINAPI FT_ComPortIdle(
-		FT_HANDLE ftHandle
-		);
-
-	FTD2XX_API
-		FT_STATUS WINAPI FT_ComPortCancelIdle(
-		FT_HANDLE ftHandle
-		);
-
-	FTD2XX_API
-		FT_STATUS WINAPI FT_VendorCmdGet(
-		FT_HANDLE ftHandle,
-		UCHAR Request,
-		UCHAR *Buf,
-		USHORT Len
-		);
-
-	FTD2XX_API
-		FT_STATUS WINAPI FT_VendorCmdSet(
-		FT_HANDLE ftHandle,
-		UCHAR Request,
-		UCHAR *Buf,
-		USHORT Len
-		);
-
-	FTD2XX_API
-		FT_STATUS WINAPI FT_VendorCmdGetEx(
-		FT_HANDLE ftHandle,
-		USHORT wValue,
-		UCHAR *Buf,
-		USHORT Len
-		);
-
-	FTD2XX_API
-		FT_STATUS WINAPI FT_VendorCmdSetEx(
-		FT_HANDLE ftHandle,
-		USHORT wValue,
-		UCHAR *Buf,
-		USHORT Len
-		);
 
 #ifdef __cplusplus
 }
