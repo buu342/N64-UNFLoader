3b18989f8aac697878b5e5599efec798a102a966
Tristan Penman <tristan@tristanpenman.com>
2021-08-28 13:09:43 +1000

    Change all aligned attributes to 16 bytes

--- Examples/Libdragon/2. USB Read/usb.c
+++ Examples/Libdragon/2. USB Read/usb.c
@@ -344,7 +344,7 @@ char usb_initialize()
 
 static void usb_findcart()
 {
-    u32 buff __attribute__((aligned(8)));
+    u32 buff __attribute__((aligned(16)));
     
     // Read the cartridge and check if we have a 64Drive.
     #ifdef LIBDRAGON
@@ -587,7 +587,7 @@ void usb_purge()
 
 static s8 usb_64drive_wait()
 {
-    u32 ret __attribute__((aligned(8)));
+    u32 ret __attribute__((aligned(16)));
     u32 timeout = 0; // I wanted to use osGetTime() but that requires the VI manager
     
     // Wait until the cartridge interface is ready
@@ -644,7 +644,7 @@ static void usb_64drive_setwritable(u8 enable)
 
 static int usb_64drive_waitidle()
 {
-    u32 status __attribute__((aligned(8)));
+    u32 status __attribute__((aligned(16)));
     u32 timeout = 0;
     do 
     {
@@ -674,7 +674,7 @@ static int usb_64drive_waitidle()
 
 static u32 usb_64drive_armstatus()
 {
-    u32 status __attribute__((aligned(8)));
+    u32 status __attribute__((aligned(16)));
     #ifdef LIBDRAGON
         status = io_read(D64_CIBASE_ADDRESS + D64_REGISTER_USBCOMSTAT);
     #else
@@ -695,7 +695,7 @@ static u32 usb_64drive_armstatus()
 
 static void usb_64drive_waitdisarmed()
 {
-    u32 status __attribute__((aligned(8)));
+    u32 status __attribute__((aligned(16)));
     do
     {
         #ifdef LIBDRAGON
@@ -814,7 +814,7 @@ static void usb_64drive_write(int datatype, const void* data, int size)
 
 static void usb_64drive_arm(u32 offset, u32 size)
 {
-    u32 ret __attribute__((aligned(8)));
+    u32 ret __attribute__((aligned(16)));
     ret = usb_64drive_armstatus();
     
     if (ret != D64_USB_ARMING && ret != D64_USB_ARMED)
@@ -872,7 +872,7 @@ static void usb_64drive_disarm()
 static u32 usb_64drive_poll()
 {
     int i;
-    u32 ret __attribute__((aligned(8)));
+    u32 ret __attribute__((aligned(16)));
     
     // Arm the USB buffer
     usb_64drive_waitidle();
@@ -955,7 +955,7 @@ static void usb_64drive_read()
 
 static void usb_everdrive_wait_pidma() 
 {
-    u32 status __attribute__((aligned(8)));
+    u32 status __attribute__((aligned(16)));
     do
     {
         status = *(volatile unsigned long *)(N64_PI_ADDRESS + N64_PI_STATUS);
@@ -1222,7 +1222,7 @@ static void usb_everdrive_write(int datatype, const void* data, int size)
 
 static u32 usb_everdrive_poll()
 {
-    char buff[16] __attribute__((aligned(8)));
+    char buff[16] __attribute__((aligned(16)));
     int len;
     int offset = 0;
     
@@ -1324,7 +1324,7 @@ static void usb_everdrive_read()
 
 static u32 usb_sc64_read_usb_scr(void)
 {
-    u32 usb_scr __attribute__((aligned(8)));
+    u32 usb_scr __attribute__((aligned(16)));
 
     #ifdef LIBDRAGON
         usb_scr = io_read(SC64_REG_USB_SCR);
@@ -1347,7 +1347,7 @@ static u32 usb_sc64_read_usb_scr(void)
 
 static u32 usb_sc64_read_usb_fifo(void)
 {
-    u32 data __attribute__((aligned(8)));
+    u32 data __attribute__((aligned(16)));
 
     #ifdef LIBDRAGON
         data = io_read(SC64_MEM_USB_FIFO_BASE);
@@ -1371,7 +1371,7 @@ static u32 usb_sc64_read_usb_fifo(void)
 
 static s8 usb_sc64_waitidle(void)
 {
-    u32 usb_scr __attribute__((aligned(8)));
+    u32 usb_scr __attribute__((aligned(16)));
 
     do
     {
@@ -1396,7 +1396,7 @@ static s8 usb_sc64_waitidle(void)
 
 static s32 usb_sc64_waitdata(u32 length)
 {
-    u32 usb_scr __attribute__((aligned(8)));
+    u32 usb_scr __attribute__((aligned(16)));
     u32 wait_length = ALIGN(MIN(length, SC64_MEM_USB_FIFO_LEN), 4);
     u32 bytes = 0;
 
@@ -1423,7 +1423,7 @@ static s32 usb_sc64_waitdata(u32 length)
 
 static void usb_sc64_setwritable(u8 enable)
 {
-    u32 scr __attribute__((aligned(8)));
+    u32 scr __attribute__((aligned(16)));
 
     #ifdef LIBDRAGON
         scr = io_read(SC64_REG_SCR);
@@ -1584,7 +1584,7 @@ static void usb_sc64_write(int datatype, const void* data, int size)
 
 static u32 usb_sc64_poll(void)
 {
-    u32 buff __attribute__((aligned(8)));
+    u32 buff __attribute__((aligned(16)));
     u32 sdram_address;
     int left;
     
