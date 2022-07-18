6760c94468bb4d73c6ba470e34744c8780b93458
CrashOveride95 <crashoveride953@gmail.com>
2022-07-18 15:02:12 -0400

    Remove nuboot.c and update entrypoints to manage stack properly

--- Examples/Libultra/2. Thread Faults/asm/entry.s
+++ Examples/Libultra/2. Thread Faults/asm/entry.s
@@ -5,25 +5,18 @@
 
 .include "macros.inc"
 
-.section .text, "ax"
+.section .start, "ax"
 
-glabel entry_point
-    la $t0, _codeSegmentBssStart
+glabel __start
+	la $t0, _codeSegmentBssStart
     la $t1, _codeSegmentBssSize
-.L80246010:
-    addi  $t1, $t1, -8
-    sw    $zero, ($t0)
-    sw    $zero, 4($t0)
-    bnez  $t1, .L80246010
-     addi  $t0, $t0, 8
-    lui   $t2, %hi(nuBoot) # $t2, 0x8024
-    lui   $sp, %hi(nuMainStack) # $sp, 0x8020
-    addiu $t2, %lo(nuBoot) # addiu $t2, $t2, 0x6dc4
-    jr    $t2
-     addiu $sp, %lo(nuMainStack) # addiu $sp, $sp, 0xa00
-    nop
-    nop
-    nop
-    nop
-    nop
+bss_clear:
+    addi $t1, $t1, -8
+    sw $zero, ($t0)
+    sw $zero, 4($t0)
+    bnez $t1, bss_clear
+	addi $t0, $t0, 8
+    la $t2, nuBoot #Boot function address
+	la $sp, nuMainStack + 0x2000 #Setup boot stack pointer, change stack size if needed here
+    jr $t2
     nop
--- Examples/Libultra/4. Command Interpreter/asm/rom_header.s
+++ Examples/Libultra/4. Command Interpreter/asm/rom_header.s
@@ -5,7 +5,7 @@
 
 .byte  0x80, 0x37, 0x12, 0x40   /* PI BSD Domain 1 register */
 .word  0x0000000F               /* Clockrate setting*/
-.word  entry_point              /* Entrypoint */
+.word  __start                  /* Entrypoint */
 
 /* SDK Revision */
 .word  0x0000144C
