0a359115cf1172f0356b05047e9498907296e548
Mateusz Faderewski <sc@mateuszfaderewski.pl>
2023-03-26 03:35:28 +0200

    SC64 code rewrite (#100)
    * SC64 rewrite
    
    * device_sc64.cpp tab->space indentation
    
    * SC64 Linux build fix

--- UNFLoader/device_sc64.h
+++ UNFLoader/device_sc64.h
@@ -13,12 +13,10 @@
     uint32_t    device_maxromsize_sc64();
     bool        device_shouldpadrom_sc64();
     bool        device_explicitcic_sc64(byte* bootcode);
-    /*
     DeviceError device_sendrom_sc64(CartDevice* cart, byte* rom, uint32_t size);
     DeviceError device_testdebug_sc64(CartDevice* cart);
-    DeviceError device_senddata_sc64(CartDevice* cart, int datatype, char *data, uint32_t size);
+    DeviceError device_senddata_sc64(CartDevice* cart, USBDataType datatype, byte* data, uint32_t size);
     DeviceError device_receivedata_sc64(CartDevice* cart, uint32_t* dataheader, byte** buff);
-    */
     DeviceError device_close_sc64(CartDevice* cart);
 
 #endif
