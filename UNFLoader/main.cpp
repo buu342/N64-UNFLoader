cb725b2c75b99fb2c2d4cd37fccfaa17d856ccd9
mallard <owlyowl202@gmail.com>
2025-03-22 21:05:42 +0000

    no luck on that macro! trying something else!

--- UNFLoader/main.cpp
+++ UNFLoader/main.cpp
@@ -72,6 +72,10 @@ static std::list<char*>  local_args;
 static std::atomic<int>  local_esclevel (0);
 static std::atomic<bool> local_reupload (false);
 
+// Local funcs
+#ifndef LINUX
+wchar_t *convertCharArrayToLPCWSTR(const char* charArray);
+#endif
 
 /*==============================
     main
@@ -487,7 +491,7 @@ static void program_loop()
                 // On windows, fopen will lock the file, so hot-reload features will not work.
                 // You have to use _fsopen and specify you want to allow shared access.
                 #ifndef LINUX
-                HANDLE file_handle = CreateFile(CA2CT(device_getrom()), GENERIC_READ, FILE_SHARE_DELETE, NULL, OPEN_EXISTING, 0, NULL);
+                HANDLE file_handle = CreateFile(convertCharArrayToLPCWSTR(device_getrom()), GENERIC_READ, FILE_SHARE_DELETE, NULL, OPEN_EXISTING, 0, NULL);
                 int file_descriptor = _open_osfhandle((intptr_t)file_handle, _O_RDONLY);
                 fp = _fdopen(file_descriptor, "r");
                 stream = std::ifstream(fp);
@@ -894,3 +898,12 @@ static void show_help()
     if (!term_isusingcurses())
         while ((category = getchar()) != '\n' && category != EOF);
 }
+
+#ifndef LINUX
+wchar_t *convertCharArrayToLPCWSTR(const char* charArray)
+{
+    wchar_t* wString=new wchar_t[4096];
+    MultiByteToWideChar(CP_ACP, 0, charArray, -1, wString, 4096);
+    return wString;
+}
+#endif
