a2c43f019a8344cbdaa4888048990cfa803c8330
Buu342 <buu342@hotmail.com>
2025-07-19 02:11:20 +0100

    Windows and MacOS CI fixes

---
+++ .github/windows/install.bat
@@ -0,0 +1,2 @@
+:: --wait doesn't work when calling the process directly, but it does if called in a batch file. Cool stuff Microsoft!
+vs_community.exe --config c:\vsconfig --passive --norestart --wait
